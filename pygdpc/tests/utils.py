7bf2f5cefa294b5fe4ce1075190bac74bb2f5a9c
ezequiel <ezequiels.90@gmail.com>
2020-10-28 17:38:34 -0300

    use randn

--- pygdpc/tests/utils.py
+++ pygdpc/tests/utils.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def gen_data(seed, k, num_series, num_periods):
     torch.manual_seed(seed)
-    data = torch.rand(num_periods, num_series)
-    f_true = torch.rand(num_periods + 1)
+    data = torch.randn(num_periods, num_series)
+    f_true = torch.randn(num_periods + 1)
     for m in range(num_series):
         data[:, m] = f_true[:num_periods] * np.sin(2 * np.pi * m / num_series) \
-                     + f_true[1:(num_periods + 1)] * (m / num_series) + 0.5 * torch.rand(num_periods)
+                     + f_true[1:(num_periods + 1)] * (m / num_series) + 0.5 * torch.randn(num_periods)
     return data
