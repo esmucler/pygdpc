d001e3114c6cb49a8380ae02db8377c2c2ab3d5c
ezequiel <ezequiels.90@gmail.com>
2020-10-28 15:53:22 -0300

    utils for tests

---
+++ pygdpc/tests/utils.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+
+
+def gen_data(seed, k, num_series, num_periods):
+    torch.manual_seed(seed)
+    data = torch.rand(num_periods, num_series)
+    f_true = torch.rand(num_periods + 1)
+    for m in range(num_series):
+        data[:, m] = f_true[:num_periods] * np.sin(2 * np.pi * m / num_series) \
+                     + f_true[1:(num_periods + 1)] * (m / num_series) + 0.5 * torch.rand(num_periods)
+    return data
